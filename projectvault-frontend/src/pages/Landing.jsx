ab7ea6dfd374cf1f5500154287d9d1f6d63bfaac
pratiksha <pratikshagaikwad857@gmail.com>
2026-03-29 00:31:53 +0530

    fix landing page why section

--- projectvault-frontend/src/pages/Landing.jsx
+++ projectvault-frontend/src/pages/Landing.jsx
@@ -144,9 +144,11 @@ const { scrollYProgress } = useScroll({
 
   {/* RIGHT SIDE */}
   <div className="why-right">
-  <h2 className="why-title">WHY</h2>
-  <h3 className="why-brand">ABHI MOTIONS</h3>
-</div>
+    <h2>
+  <span className="why-word">WHY</span><br />
+  ABHI MOTIONS
+</h2>
+  </div>
 
 </section>
 

0b4d443ec274dd08b56a8c7c990dd75b5eaa3acc
pratiksha <pratikshagaikwad857@gmail.com>
2026-03-28 23:51:31 +0530

    fix api base url

--- projectvault-frontend/src/api/api.js
+++ projectvault-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://abhimotions.onrender.com/",
+  baseURL: "https://abhimotions.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
